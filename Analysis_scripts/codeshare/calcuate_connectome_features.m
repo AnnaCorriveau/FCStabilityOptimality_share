06a0b906cf37f3d86860d9e74379de407629cf20
annacorriveau <corriveauanna@gmail.com>
2022-08-04 16:25:53 -0500

    smart indent codeshare

--- Analysis_scripts/codeshare/calcuate_connectome_features.m
+++ Analysis_scripts/codeshare/calcuate_connectome_features.m
@@ -5,7 +5,7 @@
 % input:
 % % fc_flat = Dataset with nRun cells where each cell contains the lower
 % triangle of functional connectivity for all subjects
-    % size: nSubjects x ((nNodes^2 - nNodes)/2)
+% size: nSubjects x ((nNodes^2 - nNodes)/2)
 
 % behav_scores = nSubjects x 1 matrix of performance scores (to calculate
 % optimal FC pattern)
@@ -22,7 +22,7 @@
 % run pairs, each cell size nSubjects x 1
 
 % % typicality = nRun x nRun cells of Pearson's correlation of a subjects
-% functional connectivity with the group average FC pattern 
+% functional connectivity with the group average FC pattern
 
 % % optimality = nRun x nRun cells of Pearson's correlation of a subjects
 % functional connectivity with FC of the highest performer(s)
@@ -65,7 +65,7 @@ else
 end
 fc_top = tanh(nanmean(opt_fc));
 
-% create dataset with optimal performers removed 
+% create dataset with optimal performers removed
 fc_flat_minustop = fc_flat;
 for r = 1:length(fc_flat)
     fc_flat_minustop{r}(top_log,:) = NaN;
@@ -73,55 +73,55 @@ end
 
 
 for p = 1:size(fc_flat{1},1)
-disp(p);
-   for r1 = 1:size(fc_flat,2)
-         for r2 = 1:size(fc_flat,2)
+    disp(p);
+    for r1 = 1:size(fc_flat,2)
+        for r2 = 1:size(fc_flat,2)
 
-fc_flat{r1}(isinf(fc_flat{r1})) = NaN;
-fc_flat{r2}(isinf(fc_flat{r2})) = NaN;
-fc_flat_minustop{r1}(isinf(fc_flat_minustop{r1})) = NaN;
-fc_flat_minustop{r2}(isinf(fc_flat_minustop{r2})) = NaN;
+            fc_flat{r1}(isinf(fc_flat{r1})) = NaN;
+            fc_flat{r2}(isinf(fc_flat{r2})) = NaN;
+            fc_flat_minustop{r1}(isinf(fc_flat_minustop{r1})) = NaN;
+            fc_flat_minustop{r2}(isinf(fc_flat_minustop{r2})) = NaN;
 
-               
-          run1 = fc_flat{r1}(p,:);
-          run2 = fc_flat{r2}(p,:);
-          
-          self_corr = corr(run1', run2', 'rows','pairwise');
 
-          self_FC = tanh(nanmean([run1; run2])');
-            
-          others1 = fc_flat{r1};
-               others1(p,:) = [];
-          others2 = fc_flat{r2};
-               others2(p,:) = [];
+            run1 = fc_flat{r1}(p,:);
+            run2 = fc_flat{r2}(p,:);
 
-           cat_others = cat(1, others1, others2);
+            self_corr = corr(run1', run2', 'rows','pairwise');
 
-           others_FC = tanh(nanmean(cat_others)');
+            self_FC = tanh(nanmean([run1; run2])');
 
-           typicality{r1,r2}(p,1) = corr(self_FC, others_FC,'rows','pairwise');
-           stability{r1,r2}(p,:) = self_corr;
-           
-           tmpstab(r1,r2) = self_corr;
-     
-           % Optimality analysis
+            others1 = fc_flat{r1};
+            others1(p,:) = [];
+            others2 = fc_flat{r2};
+            others2(p,:) = [];
+
+            cat_others = cat(1, others1, others2);
+
+            others_FC = tanh(nanmean(cat_others)');
+
+            typicality{r1,r2}(p,1) = corr(self_FC, others_FC,'rows','pairwise');
+            stability{r1,r2}(p,:) = self_corr;
+
+            tmpstab(r1,r2) = self_corr;
+
+            % Optimality analysis
             run1_top = fc_flat_minustop{r1}(p,:);
             run2_top = fc_flat_minustop{r2}(p,:);
             self_FC_top = tanh(nanmean([run1_top; run2_top])');
             optimality{r1,r2}(p,1) = corr(self_FC_top, fc_top' ,'rows','pairwise');
 
 
-  
-end % for r1 = 1:size(all_runs,2)
-end % for r2 = 1:size(all_runs,2)
-%  
-end
-    
 
- for r1 = 1:size(fc_flat,2)
-         for r2 = 1:size(fc_flat,2)    
-             discriminability{r1,r2} = atanh(stability{r1,r2})./atanh(typicality{r1,r2});
-         end
- end
-        
+        end % for r1 = 1:size(all_runs,2)
+    end % for r2 = 1:size(all_runs,2)
+    %
+end
+
+
+for r1 = 1:size(fc_flat,2)
+    for r2 = 1:size(fc_flat,2)
+        discriminability{r1,r2} = atanh(stability{r1,r2})./atanh(typicality{r1,r2});
+    end
+end
+
 
