cc32992d8df6ce256384e6d3f6466eaebf322496
annacorriveau <corriveauanna@gmail.com>
2022-08-09 14:38:50 -0500

    Clean up R script

--- Analysis_scripts/manuscriptanalysis/Step4_FC_StabTypOptDisc_Models.R
+++ Analysis_scripts/manuscriptanalysis/Step4_FC_StabTypOptDisc_Models.R
@@ -25,8 +25,6 @@ dfmot <- scale(df[8:9])
 df <- cbind(df5, dataset, dfmot)
 
 df <- data.frame(df)
-#setnames(df, new = names)
-
 
 # Delete any NAs so we can compare across all variables
 df <- df[!is.na(df$behav),]
@@ -39,22 +37,18 @@ df <- df[!is.na(df$disc),]
 
 # does stability predict SA performance across datasets?
 model_SA_stab <- lmer(behav ~ stab + mot_avg + mot_diff + (1 | dataset), data=df)
-#anova(model_SA_stab)
 summary(model_SA_stab)
 
 # does typicality predict SA performance across datasets?
 model_SA_typ <- lmer(behav ~ typ + mot_avg + mot_diff + (1 | dataset), data=df)
-#anova(model_SA_typ)
 summary(model_SA_typ)
 
 # does optimality predict SA performance across datasets?
 model_SA_opt <- lmer(behav ~ opt + mot_avg + mot_diff +  (1 | dataset), data=df)
-#anova(model_SA_opt)
 summary(model_SA_opt)
 
 # does discriminability predict SA performance across datasets?
 model_SA_disc <- lmer(behav ~ disc + mot_avg + mot_diff + (1 | dataset), data=df)
-#anova(model_SA_disc)
 summary(model_SA_disc)
 
 
@@ -73,23 +67,22 @@ summary(model_SA_stabopt)
 
 AIC(model_SA_stab, model_SA_typ, model_SA_opt, model_SA_disc, model_SA_stab_typ, model_SA_stabtyp, model_SA_stab_opt, model_SA_stabopt)
 
+# Are any models significantly better than others?
 anova(model_SA_opt, model_SA_stab_opt)
 anova(model_SA_opt, model_SA_stabopt)
-
 anova(model_SA_opt, model_SA_stab)
-# ========================================================================
+
+
 # ========================================================================
 # Working memory
+# ========================================================================
 dw <- read.table('/Users/annacorriveau/Documents/GitHub/FCStability_Typicality/outputs/FC_DOTS_GLMinput_WM_NoNorm.txt')
 names <- c('behav', 'stab', 'typ', 'disc', 'opt', 'dataset','wmCPM_strength','mot_avg','mot_diff')
 setnames(dw, new = names)
 dataset <- factor(dw$dataset)
 dw5 <- scale(dw[1:5])
 dwmot <- scale(dw[8:9])
-#dw5 <- dw[1:5]
-#dwmot <- dw[8:9]
 dw <- cbind(dw5, dataset, dwmot)
-
 dw <- data.frame(dw)
 
 # Delete any NAs so we can compare across all variables
@@ -102,22 +95,19 @@ dw <- dw[!is.na(dw$disc),]
 
 # does stability predict WM performance across datasets?
 model_WM_stab <- lmer(behav ~ stab + mot_avg + mot_diff + (1 | dataset), data=dw)
-#anova(model_WM_stab)
 summary(model_WM_stab)
 
 # does typicality predict WM performance across datasets?
 model_WM_typ <- lmer(behav ~ typ + mot_avg + mot_diff + (1 | dataset), data=dw)
-#anova(model_WM_typ)
 summary(model_WM_typ)
 
 
-# does optimality predict SA performance across datasets?
+# does optimality predict WM performance across datasets?
 model_WM_opt <- lmer(behav ~ opt + mot_avg + mot_diff + (1 | dataset), data=dw)
-#anova(model_WM_opt)
 summary(model_WM_opt)
 
 
-# does discriminability predict SA performance across datasets?
+# does discriminability predict WM performance across datasets?
 model_WM_disc <- lmer(behav ~ disc + mot_avg + mot_diff + (1 | dataset), data=dw)
 #anova(model_WM_disc)
 summary(model_WM_disc)
@@ -137,8 +127,10 @@ model_WM_stabopt <- lmer(behav ~ stab * opt + mot_avg + mot_diff +  (1 | dataset
 summary(model_WM_stabopt)
 
 
+# Model comparison
 AIC(model_WM_stab, model_WM_typ, model_WM_opt, model_WM_disc, model_WM_stab_typ, model_WM_stabtyp, model_WM_stab_opt, model_WM_stabopt)
 
+# Are models significantly better than others?
 anova(model_WM_stab, model_WM_stab_opt)
 anova(model_WM_stab, model_WM_stabopt)
 
@@ -171,7 +163,6 @@ dw <- dw[!is.na(dw$stab),]
 dw <- dw[!is.na(dw$typ),]
 dw <- dw[!is.na(dw$opt),]
 dw <- dw[!is.na(dw$disc),]
-#as.factor(df$dataset)
 
 
 dw2 <- dw[dw$dataset == 2,]
@@ -240,113 +231,38 @@ model_WM2_wmCPM_opt <- glm(behav~wmCPM_strength*opt + mot_avg + mot_diff, data =
 summary(model_WM2_wmCPM_opt)
 
 
-# ==============================================================
-# ++++++ RESPONSE TO REVIEWS +++++++++++++++++++++++++++++++++++
-# ==============================================================
-# clear 
-rm(list=ls())
 
+ 
+#===============================================================================
+# Model Identifiability with stability and typicality 
+#
+df <- read.table('/Users/annacorriveau/Documents/GitHub/FCStability_Typicality/outputs/FC_DOTS_GLMinput_SA_NoNorm.txt')
+ID_ATTN <- read.table('/Users/annacorriveau/Documents/GitHub/FCStability_Typicality/outputs/FC_DOTS_ID_ATTN.txt')
+ID <- ID_ATTN$V1
+names <- c('behav', 'stab', 'typ', 'disc', 'opt', 'dataset','saCPM_strength','mot_avg','mot_diff','ID')
+setnames(df, new = names)
+dataset <- factor(df$dataset)
+df5 <- scale(df[1:5])
+dfmot <- scale(df[8:9])
+df <- cbind(df5, dataset, dfmot, scale(ID))
+df <- data.frame(df)
 
-df_review <- read.table('/Users/annacorriveau/Documents/GitHub/FCStability_Typicality/outputs/FC_DOTS_GLMinput_SA.txt')
-names <- c('behav', 'stab', 'typ', 'disc', 'opt', 'dataset','saCPM_strength')
-setnames(df_review, new = names)
-ds_review <- factor(df_review$dataset)
-#df <- scale(df[1:5])
-df_review <- cbind(df_review, ds_review)
+model_ID_attn <- glm(ID ~ stab + typ + disc, data=df)
+summary (model_ID_attn)
+ 
 
-df_review <- data.frame(df_review)
-
-# Delete any NAs so we can compare across all variables
-df_review <- df_review[!is.na(df_review$behav),]
-df_review <- df_review[!is.na(df_review$stab),]
-df_review <- df_review[!is.na(df_review$typ),]
-df_review <- df_review[!is.na(df_review$opt),]
-df_review <- df_review[!is.na(df_review$disc),]
-
-
-# does stability predict SA performance across datasets?
-model_SA_stab_review <- lmer(behav ~ stab + mot_avg + mot_diff + (1 | dataset), data=df_review)
-#anova(model_SA_stab)
-summary(model_SA_stab_review)
-
-# does typicality predict SA performance across datasets?
-model_SA_typ_review <- lmer(behav ~ typ + mot_avg + mot_diff + (1 | dataset), data=df_review)
-#anova(model_SA_typ)
-summary(model_SA_typ_review)
-
-# does discriminability predict SA performance across datasets?
-model_SA_disc_review <- lmer(behav ~ disc + mot_avg + mot_diff + (1 | dataset), data=df_review)
-#anova(model_SA_disc)
-summary(model_SA_disc_review)
-
-# does optimality predict SA performance across datasets?
-model_SA_opt_review <- lmer(behav ~ opt + mot_avg + mot_diff + (1 | dataset), data=df_review)
-#anova(model_SA_opt)
-summary(model_SA_opt_review)
-
-model_SA_stab_typ_review <- lmer(behav ~ stab + typ + mot_avg + mot_diff + (1 | dataset), data=df_review)
-summary(model_SA_stab_typ_review)
-
-model_SA_stabtyp_review <- lmer(behav ~ stab * typ + mot_avg + mot_diff + (1 | dataset), data=df_review)
-summary(model_SA_stabtyp_review)
-
-model_SA_stab_opt_review <- lmer(behav ~ stab + opt + mot_avg + mot_diff + (1 | dataset), data=df_review)
-summary(model_SA_stab_opt_review)
-
-model_SA_stabopt_review <- lmer(behav ~ stab * opt + mot_avg + mot_diff + (1 | dataset), data=df_review)
-summary(model_SA_stabopt_review)
-
-
-# ++++++++++++++++++++++++++++++++++++
-# ++++++++++++++++++++++++++++++++++++
-# Working memory
 dw <- read.table('/Users/annacorriveau/Documents/GitHub/FCStability_Typicality/outputs/FC_DOTS_GLMinput_WM.txt')
-names <- c('behav', 'stab', 'typ', 'disc', 'opt', 'dataset','wmCPM_strength')
+ID_WM <- read.table('/Users/annacorriveau/Documents/GitHub/FCStability_Typicality/outputs/FC_DOTS_ID_WM.txt')
+ID <- ID_WM$V1
+names <- c('behav', 'stab', 'typ', 'disc', 'opt', 'dataset','wmCPM_strength','mot_avg','mot_diff')
 setnames(dw, new = names)
-ds <- factor(dw$dataset)
-#dw <- scale(dw[1:5])
-dw <- cbind(dw, ds)
-
+dataset <- factor(dw$dataset)
+dw5 <- scale(dw[1:5])
+dwmot <- scale(dw[8:9])
+dw <- cbind(dw5, dataset, dwmot, scale(ID))
 dw <- data.frame(dw)
 
-# Delete any NAs so we can compare across all variables
-dw <- dw[!is.na(dw$behav),]
-dw <- dw[!is.na(dw$stab),]
-dw <- dw[!is.na(dw$typ),]
-dw <- dw[!is.na(dw$opt),]
-dw <- dw[!is.na(dw$disc),]
 
-
-# does stability predict WM performance across datasets?
-model_WM_stab <- lmer(behav ~ stab + mot_avg + mot_diff + (1 | dataset), data=dw)
-#anova(model_WM_stab)
-summary(model_WM_stab)
-
-# does typicality predict WM performance across datasets?
-model_WM_typ <- lmer(behav ~ typ + mot_avg + mot_diff + (1 | dataset), data=dw)
-#anova(model_WM_typ)
-summary(model_WM_typ)
-
-# does discriminability predict SA performance across datasets?
-model_WM_disc <- lmer(behav ~ disc + mot_avg + mot_diff + (1 | dataset), data=dw)
-#anova(model_WM_disc)
-summary(model_WM_disc)
-
-# does optimality predict SA performance across datasets?
-model_WM_opt <- lmer(behav ~ opt + mot_avg + mot_diff + (1 | dataset), data=dw)
-#anova(model_WM_opt)
-summary(model_WM_opt)
-
-model_WM_stab_typ <- lmer(behav ~ stab + typ + mot_avg + mot_diff + (1 | dataset), data=dw)
-summary(model_WM_stab_typ)
-
-model_WM_stabtyp <- lmer(behav ~ stab * typ + mot_avg + mot_diff + (1 | dataset), data=dw)
-summary(model_WM_stabtyp)
-
-model_WM_stab_opt <- lmer(behav ~ stab + opt + mot_avg + mot_diff + (1 | dataset), data=dw)
-summary(model_WM_stab_opt)
-
-model_WM_stabopt <- lmer(behav ~ stab * opt + mot_avg + mot_diff + (1 | dataset), data=dw)
-summary(model_WM_stabopt)
-
+model_ID_wm <- glm(ID ~ stab + typ + disc, data=dw)
+summary(model_ID_wm)
 
